24ef6d9fd1f4ed468d46c8fe6677731dc1848ed3
Sam Mohamed <sam@pydevs.org>
2012-12-04 04:28:43 -0800

    removed prod settings

--- src/bvd/settings_local.py
+++ src/bvd/settings_local.py
@@ -83,7 +83,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/opt/bvd/src/bvd/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -109,8 +109,7 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '4_6gzfownx-wvv8x&mprtbs%7ii!brbz00o7!)mmx9=z==$vl+'
+
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
