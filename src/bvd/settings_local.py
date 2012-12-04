29d1fcc5e50f9096eca007036e91471d34d5e9da
Sam Mohamed <sam@pydevs.org>
2012-12-04 04:22:37 -0800

    removing prod settings

--- src/bvd/settings_local.py
+++ src/bvd/settings_local.py
@@ -32,22 +32,8 @@ DEBUG = True
 USE_SSL = False
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
 MANAGERS = ADMINS
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': '', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': '',                      # Or path to database file if using sqlite3.
-#         'USER': '',                      # Not used with sqlite3.
-#         'PASSWORD': '',                  # Not used with sqlite3.
-#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#     }
-# }
 
 DATABASES = {
     'default': {
